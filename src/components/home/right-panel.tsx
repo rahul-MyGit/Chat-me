c55c3a15db50e972e5403bb267adaf2f6f027138
Rahul <rahulhero93111@gmail.com>
2024-05-22 23:06:08 +0530

    added

--- src/components/home/right-panel.tsx
+++ src/components/home/right-panel.tsx
@@ -2,11 +2,7 @@ import React from 'react'
 
 function RightPanel() {
   return (
-<<<<<<< HEAD
     <div>right-panel</div>
-=======
-    <div>right-panel</div>
->>>>>>> bc2fd547832fd339cffd05a24a61efe359e3b59e
   )
 }
 
