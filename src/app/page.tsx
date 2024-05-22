94ca1f437640f34838267016755435c2481ada92
Rahul <rahulhero93111@gmail.com>
2024-05-23 00:13:53 +0530

    text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
       <div className='flex overflow-y-hidden h-[calc(100vh-50px)] max-w-[1700px] mx-auto bg-left-panel'>
         {/* Green background decorator for Light Mode */}
         <div className='fixed top-0 left-0 w-full h-36 bg-green-primary dark:bg-transparent -z-30' />
-        <LeftPanel />
+        <LeftPanel /> mid
         <RightPanel />
       </div>
     </main>
