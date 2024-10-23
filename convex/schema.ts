5e4eddc8d6052ac5ea1a2f63717a9dbb8a1bf19c
Rahul <rahul.mymail1@gmail.com>
2024-10-23 13:23:13 +0530

    Add: table and BE mutated function for conversation

--- convex/schema.ts
+++ convex/schema.ts
@@ -17,5 +17,12 @@ export default defineSchema({
         groupName: v.optional(v.string()),
         groupImage: v.optional(v.string()),
         admin: v.optional(v.id('users'))
-    })
+    }),
+
+    messages: defineTable({
+        conversation: v.id('conversations'),
+        sender: v.string(),    //coz for gpt
+        content: v.string(),
+        messageType: v.union(v.literal('text'), v.literal('image'), v.literal('video')),
+    }).index('by_conversation', ["conversation"])
 });
