6996f47bcf770a19ad6bebbd83b93a7718816787
Rahul <rahul.mymail1@gmail.com>
2024-10-25 00:31:33 +0530

    Add: store and fix ui

--- src/components/home/right-panel.tsx
+++ src/components/home/right-panel.tsx
@@ -5,13 +5,14 @@ import MessageInput from "./messageInput";
 import MessageContainer from "./messageContainer";
 import ChatPlaceHolder from "./chatPlaceholder";
 import GroupMembersDialog from "./groupMemberDialog";
+import { useConvoStore } from "@/store/chat-store";
 
 const RightPanel = () => {
-	const selectedConversation = null;
-  const isGroup = true;
-	if (selectedConversation) return <ChatPlaceHolder />;
+	const {selectedConversation, setSelectedConversation} = useConvoStore();
+	if (!selectedConversation) return <ChatPlaceHolder />;
 
-	const conversationName = "John Doe";
+	const conversationName = selectedConversation.groupName || selectedConversation.name;
+	const conversationImage = selectedConversation.groupImage || selectedConversation.image;
 
 	return (
 		<div className='w-3/4 flex flex-col'>
@@ -20,14 +21,14 @@ const RightPanel = () => {
 				<div className='flex justify-between bg-gray-primary p-3'>
 					<div className='flex gap-3 items-center'>
 						<Avatar>
-							<AvatarImage src={"/placeholder.png"} className='object-cover' />
+							<AvatarImage src={ conversationImage ||"/placeholder.png"} className='object-cover' />
 							<AvatarFallback>
 								<div className='animate-pulse bg-gray-tertiary w-full h-full rounded-full' />
 							</AvatarFallback>
 						</Avatar>
 						<div className='flex flex-col'>
 							<p>{conversationName}</p>
-							{isGroup && <GroupMembersDialog />}
+							{selectedConversation.isGroup && <GroupMembersDialog />}
 						</div>
 					</div>
 
@@ -35,7 +36,7 @@ const RightPanel = () => {
 						<a href='/video-call' target='_blank'>
 							<Video size={23} />
 						</a>
-						<X size={16} className='cursor-pointer' />
+						<X size={16} className='cursor-pointer' onClick={() => setSelectedConversation(null)} />
 					</div>
 				</div>
 			</div>
