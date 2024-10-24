703a5c07eda3f3a5b1dc17da1e145336eaba4f1b
Rahul <rahul.mymail1@gmail.com>
2024-10-25 03:18:16 +0530

    Add: messages in convex

---
+++ convex/messages.ts
@@ -0,0 +1,39 @@
+import { ConvexError, v } from "convex/values";
+import { mutation } from "./_generated/server";
+
+
+export const sendTextMessage = mutation({
+    args: {
+        sender: v.string(),
+        context: v.string(),
+        conversation: v.id('conversations')
+    },
+    handler: async (ctx, args) => {
+        const identity = await ctx.auth.getUserIdentity();
+        if(!identity){
+            throw new ConvexError(" not authorized");
+        }
+
+        const user = await ctx.db.query('users').withIndex('by_tokenIdentifier', (q) => q.eq('tokenIdentifier', identity.tokenIdentifier)).unique();
+
+        if(!user){
+            throw new ConvexError("User not found");
+        }
+
+        const conversation = await ctx.db.query('conversations').filter(q => q.eq(q.field('_id'), args.conversation)).first();
+        if(!conversation){
+            throw new ConvexError("Conversation not found");
+        }
+
+        if(!conversation.participants.includes(user._id)){
+            throw new ConvexError("User not in conversation");
+        }
+
+        await ctx.db.insert('messages', {
+            sender: args.sender,
+            content: args.context,
+            conversation: args.conversation,
+            messageType: 'text'
+        })
+    }
+})
