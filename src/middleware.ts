0e4d01e86ced253b44f9ac86b245118e8f4f3c71
Rahul <rahul.mymail1@gmail.com>
2024-10-25 07:00:45 +0530

    fix: rebase

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,17 +1,13 @@
-// import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-import { clerkMiddleware } from "@clerk/nextjs/server";
-
-// const isProtectedRoute = createRouteMatcher([
-//     '/(.*)',
-//   ]);
-// export default clerkMiddleware((auth,req)=>{
-//     if( isProtectedRoute(req)) {
-//         auth().protect();
-//     }
-// });
-
-export default clerkMiddleware({});
+const isProtectedRoute = createRouteMatcher([
+    '/(.*)',
+  ]);
+export default clerkMiddleware((auth,req)=>{
+    if( isProtectedRoute(req)) {
+        auth().protect();
+    }
+});
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
