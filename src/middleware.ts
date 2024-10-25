fab31a292e3b2580a0e2c372bed4597ff9df7dfd
Rahul <rahul.mymail1@gmail.com>
2024-10-25 06:56:55 +0530

    Deploy check

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,13 +1,17 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+// import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher([
-    '/(.*)',
-  ]);
-export default clerkMiddleware((auth,req)=>{
-    if( isProtectedRoute(req)) {
-        auth().protect();
-    }
-});
+import { clerkMiddleware } from "@clerk/nextjs/server";
+
+// const isProtectedRoute = createRouteMatcher([
+//     '/(.*)',
+//   ]);
+// export default clerkMiddleware((auth,req)=>{
+//     if( isProtectedRoute(req)) {
+//         auth().protect();
+//     }
+// });
+
+export default clerkMiddleware({});
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
