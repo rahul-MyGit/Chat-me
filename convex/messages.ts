8ba9cf91758c1ffbd2b32594b023ae53f9cad66f
Rahul <rahul.mymail1@gmail.com>
2024-11-04 01:55:33 +0530

    Add: mutation for saving gpt msg to DB

--- convex/messages.ts
+++ convex/messages.ts
@@ -1,5 +1,6 @@
 import { ConvexError, v } from "convex/values";
 import { mutation, query } from "./_generated/server";
+import { api } from "./_generated/api";
 
 export const sendTextMessage = mutation({
   args: {
@@ -45,13 +46,32 @@ export const sendTextMessage = mutation({
 
 
     //AI:
-    
-    if(args.content.startsWith('@gpt')){
-      await ctx.scheduler.runAfter(0, api.openai.chat)
+
+    if (args.content.startsWith('@gpt')) {
+      await ctx.scheduler.runAfter(0, api.openai.chat, {
+        messageBody: args.content,
+        conversation: args.conversation
+      })
     }
 
   },
 });
+
+export const sendChatGPTMessage = mutation({
+  args: {
+    content: v.string(),
+    conversation: v.id("conversations"),
+  },
+  handler: async (ctx, args) => {
+    await ctx.db.insert("messages", {
+      content: args.content,
+      sender: "ChatGPT",
+      messageType: "text",
+      conversation: args.conversation,
+    });
+  },
+});
+
 
 export const getMessages = query({
   args: {
---
+++ convex/openai.ts
@@ -0,0 +1,39 @@
+import { v } from 'convex/values';
+import OpenAI from 'openai';
+import { action } from './_generated/server';
+import { api } from './_generated/api';
+
+
+
+const apiKey = process.env.OPEN_API_KEY
+
+const openai = new OpenAI({apiKey});
+
+export const chat = action({
+    args: {
+        messageBody: v.string(),
+        conversation: v.id("conversations")
+    },
+    handler: async (ctx, args) => {
+        const res = await openai.chat.completions.create({
+            model: 'gpt-3.5-turbo',
+            messages: [
+                {
+                    'role': "system",
+                    content: "You are a terse bot in a group chat responding to questions with 1-sentence answers"
+                }, 
+                {
+                    role:'user',
+                    content:args.messageBody
+                }
+            ]
+        })
+
+        const messsageContent = res.choices[0].message.content
+
+        await ctx.runMutation(api.messages.sendChatGPTMessage, {
+            content: messsageContent ?? "I'm sorry, I don't know that",
+            conversation: args.conversation
+        })
+    }
+})
