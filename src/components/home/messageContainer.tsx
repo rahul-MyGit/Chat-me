f45e2fa991370e969be9db84ece169e106517ffc
Rahul <rahul.mymail1@gmail.com>
2024-11-04 18:53:44 +0530

    Added the DALL-E suppor from Images

--- src/components/home/messageContainer.tsx
+++ src/components/home/messageContainer.tsx
@@ -3,6 +3,8 @@ import ChatBubble from "./chatBubble";
 import { useQuery } from "convex/react";
 import { api } from "../../../convex/_generated/api";
 import { useConvoStore } from "@/store/chat-store";
+import { useEffect, useRef } from "react";
+
 
 const MessageContainer = () => {
   const { selectedConversation } = useConvoStore();
@@ -12,16 +14,26 @@ const MessageContainer = () => {
 
   const me = useQuery(api.users.getMe);
 
-  return (
-    <div className="relative p-3 flex-1 overflow-auto h-full bg-chat-tile-light dark:bg-chat-tile-dark">
-      <div className="mx-12 flex flex-col gap-3 h-full">
-        {messages?.map((msg, idx) => (
-          <div key={msg._id}>
-            <ChatBubble me={me} message={msg} previousMessage={idx > 0 ? messages[idx - 1] : undefined} />
-          </div>
-        ))}
-      </div>
+  const lastMessageRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    setTimeout(() => {
+      lastMessageRef.current?.scrollIntoView({ behavior: 'smooth' });
+    }, 100);
+}, [messages])
+
+
+
+return (
+  <div className="relative p-3 flex-1 overflow-auto h-full bg-chat-tile-light dark:bg-chat-tile-dark">
+    <div className="mx-12 flex flex-col gap-3">
+      {messages?.map((msg, idx) => (
+        <div key={msg._id} ref={lastMessageRef}>
+          <ChatBubble me={me} message={msg} previousMessage={idx > 0 ? messages[idx - 1] : undefined} />
+        </div>
+      ))}
     </div>
-  );
+  </div>
+);
 };
 export default MessageContainer;
